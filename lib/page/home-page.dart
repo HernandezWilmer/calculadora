1c00e637ef238f9c3050a4159deecc9494b3c4da
Wilmer H <wilmer.hernandez@gmail.com>
2021-04-23 21:26:46 -0500

    operaciones

--- lib/page/home-page.dart
+++ lib/page/home-page.dart
@@ -18,27 +18,40 @@ class _HomePageState extends State<HomePage> {
   Widget _body() {
     return Column(
       children: [
-        Container(
-          color: Colors.red,
+        Expanded(
+          child: Container(
+            color: Colors.red,
+          ),
         ),
         Container(
           color: Colors.blue,
           height: 100,
+          child: Row(
+            children: [    
+              Text(operaciones),
+            ],
+          ),
         ),
+      
         Container(
             color: Colors.yellow,
             child: Column(
               children: [
                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
                   children: [
-                    ElevatedButton(onPressed: () {}, child: Text("0")),
-                    ElevatedButton(onPressed: () {}, child: Text("C")),
-                    ElevatedButton(onPressed: () {}, child: Text("=")),
-                    ElevatedButton(onPressed: () {}, child: Text("+")),
+                    ElevatedButton(onPressed: () {}, child: Text("7")),
+                    ElevatedButton(onPressed: () {}, child: Text("8")),
+                    ElevatedButton(onPressed: () {}, child: Text("9")),
+                    ElevatedButton(onPressed: () {}, child: Text("/")),
                   ],
-                )
-              ],
-            ),
-          },
-    },
-  }
+                ),
+                 Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceAround,
+                  children: [
+                    ElevatedButton(onPressed: () {}, child: Text("4")),
+                    ElevatedButton(onPressed: () {}, child: Text("8")),
+                    ElevatedButton(onPressed: () {}, child: Text("6")),
+                    ElevatedButton(onPressed: () {}, child: Text("/")),
+                  ],
+                ),
